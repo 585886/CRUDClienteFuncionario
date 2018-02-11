4ba37985c54084eb9840c487446acf2cad1ede00
Alan Vinicius Developer <alan123vinicius@gmail.com>
2018-02-11 20:38:14 -0200

    Add files via upload

--- funcionarios/edit.php
+++ funcionarios/edit.php
@@ -9,64 +9,64 @@
   <div class="row">
     <div class="form-group col-md-7">
       <label for="name">Nome:</label>
-      <input type="text" class="form-control" name="customer['nome']" value="<?php echo $customer['nome']; ?>">
+      <input type="text" class="form-control" name="customer['nome']" value="<?php echo $customer['nome']; ?>" required>
     </div>
 
     <div class="form-group col-md-3">
       <label for="campo2">Endereço:</label>
-      <input type="text" class="form-control" name="customer['endereco']" value="<?php echo $customer['endereco']; ?>">
+      <input type="text" class="form-control" name="customer['endereco']" value="<?php echo $customer['endereco']; ?>" required>
     </div>
 
     <div class="form-group col-md-2">
       <label for="campo3">CPF:</label>
-      <input type="text" class="form-control" name="customer['CPF']" value="<?php echo $customer['CPF']; ?>">
+      <input type="text" class="form-control" name="customer['CPF']" value="<?php echo $customer['CPF']; ?>" required>
     </div>
   </div>
   <div class="row">
     <div class="form-group col-md-5">
       <label for="campo1">Data de Nascimento:</label>
-      <input type="date" class="form-control" name="customer['dataNascimento']" value="<?php echo $customer['dataNascimento']; ?>">
+      <input type="date" class="form-control" name="customer['dataNascimento']" value="<?php echo $customer['dataNascimento']; ?>" required>
     </div>
 
     <div class="form-group col-md-3">
       <label for="campo2">CEP:</label>
-      <input type="text" class="form-control" name="customer['CEP']" value="<?php echo $customer['CEP']; ?>">
+      <input type="text" class="form-control" name="customer['CEP']" value="<?php echo $customer['CEP']; ?>" required>
     </div>
 
     <div class="form-group col-md-2">
       <label for="campo3">Municipio:</label>
-      <input type="text" class="form-control" name="customer['cidade']" value="<?php echo $customer['cidade']; ?>">
+      <input type="text" class="form-control" name="customer['cidade']" value="<?php echo $customer['cidade']; ?>" required>
     </div>
 
     <div class="form-group col-md-2">
       <label for="campo3">Data de Modificação</label>
-      <input type="date" class="form-control" name="customer['created']" disabled value="<?php echo $customer['created']; ?>">
+      <input type="date" class="form-control" name="customer['created']" disabled value="<?php echo $customer['created']; ?>" required>
     </div>
   </div>
   <div class="row">
     <div class="form-group col-md-3">
       <label for="campo1">Município:</label>
-      <input type="text" class="form-control" name="customer['cidade']" value="<?php echo $customer['cidade']; ?>">
+      <input type="text" class="form-control" name="customer['cidade']" value="<?php echo $customer['cidade']; ?>" required>
     </div>
 
     <div class="form-group col-md-2">
       <label for="campo2">UF:</label>
-      <input type="text" class="form-control" name="customer['estado']" value="<?php echo $customer['estado']; ?>">
+      <input type="text" class="form-control" name="customer['estado']" value="<?php echo $customer['estado']; ?>" required>
     </div>
 
     <div class="form-group col-md-2">
       <label for="campo3">Celular</label>
-      <input type="text" class="form-control" name="customer['celular']" value="<?php echo $customer['celular']; ?>">
+      <input type="text" class="form-control" name="customer['celular']" value="<?php echo $customer['celular']; ?>" required>
     </div>
 
 	<div class="form-group col-md-2">
       <label for="campo3">Bairro</label>
-      <input type="text" class="form-control" name="customer['bairro']" value="<?php echo $customer['bairro']; ?>">
+      <input type="text" class="form-control" name="customer['bairro']" value="<?php echo $customer['bairro']; ?>" required>
     </div>
 
 	<div class="form-group col-md-2">
       <label for="campo3">RG:</label>
-      <input type="text" class="form-control" name="customer['RG']" value="<?php echo $customer['RG']; ?>">
+      <input type="text" class="form-control" name="customer['RG']" value="<?php echo $customer['RG']; ?>" required >
     </div>
 	
    
