6ff5cc27286752a03f590a613e43cc70fe0d4669
Alan Vinicius Developer <alan123vinicius@gmail.com>
2018-02-11 20:43:04 -0200

    Add files via upload

--- clientes/add.php
+++ clientes/add.php
@@ -6,40 +6,40 @@
 
 <h2 style = "color:white;">Novo Cliente</h2>
 
-<form style = "color:white;"action="add.php" method="post">
+<form style = "color:white;"action="add.php" method="post" required>
   <!-- area de campos do form -->
   <hr />
   <div class="row">
     <div class="form-group col-md-7">
       <label for="name">Nome</label>
-      <input type="text" class="form-control" name="customer['nome']">
+      <input type="text" class="form-control" name="customer['nome']" required>
     </div>
 
     <div class="form-group col-md-3">
       <label for="campo2">CPF</label>
-      <input type="text" class="form-control" name="customer['CPF']">
+      <input type="text" class="form-control" name="customer['CPF']" required>
     </div>
 
     <div class="form-group col-md-2">
       <label for="campo3">Data de Nascimento</label>
-      <input type="date" class="form-control" name="customer['dataNascimento']">
+      <input type="date" class="form-control" name="customer['dataNascimento']" required>
     </div>
   </div>
   
   <div class="row">
     <div class="form-group col-md-5">
       <label for="campo1">Endereço</label>
-      <input type="text" class="form-control" name="customer['endereco']">
+      <input type="text" class="form-control" name="customer['endereco']" required>
     </div>
 
     <div class="form-group col-md-3">
       <label for="campo2">Bairro</label>
-      <input type="text" class="form-control" name="customer['bairro']">
+      <input type="text" class="form-control" name="customer['bairro']"required>
     </div>
     
     <div class="form-group col-md-2">
       <label for="campo3">CEP</label>
-      <input type="text" class="form-control" name="customer['CEP']">
+      <input type="text" class="form-control" name="customer['CEP']"required>
     </div>
     
     <div class="form-group col-md-2">
@@ -51,27 +51,27 @@
   <div class="row">
     <div class="form-group col-md-3">
       <label for="campo1">Município</label>
-      <input type="text" class="form-control" name="customer['cidade']">
+      <input type="text" class="form-control" name="customer['cidade']" required>
     </div>
     
     <div class="form-group col-md-2">
       <label for="campo2">Telefone</label>
-      <input type="text" class="form-control" name="customer['telefoneFixo']">
+      <input type="text" class="form-control" name="customer['telefoneFixo']" required>
     </div>
     
     <div class="form-group col-md-2">
       <label for="campo3">Celular</label>
-      <input type="text" class="form-control" name="customer['celular']">
+      <input type="text" class="form-control" name="customer['celular']"required>
     </div>
     
     <div class="form-group col-md-1">
       <label for="campo3">UF</label>
-      <input type="text" class="form-control" name="customer['estado']">
+      <input type="text" class="form-control" name="customer['estado']" required>
     </div>
     
     <div class="form-group col-md-2">
       <label for="campo3">Inscrição Estadual</label>
-      <input type="text" class="form-control" name="customer['ie']">
+      <input type="text" class="form-control" name="customer['ie']" required>
     </div>
     
  
--- clientes/edit.php
+++ clientes/edit.php
@@ -12,64 +12,64 @@
   <div class="row">
     <div class="form-group col-md-7">
       <label for="name">Nome</label>
-      <input type="text" class="form-control" name="customer['nome']" value="<?php echo $customer['nome']; ?>">
+      <input type="text" class="form-control" name="customer['nome']" value="<?php echo $customer['nome']; ?>" required>
     </div>
 
     <div class="form-group col-md-3">
       <label for="campo2">CPF</label>
-      <input type="text" class="form-control" name="customer['CPF']" value="<?php echo $customer['CPF']; ?>">
+      <input type="text" class="form-control" name="customer['CPF']" value="<?php echo $customer['CPF']; ?>"required>
     </div>
 
     <div class="form-group col-md-2">
       <label for="campo3">Data de Nascimento</label>
-      <input type="date" class="form-control" name="customer['dataNascimento']" value="<?php echo $customer['dataNascimento']; ?>">
+      <input type="date" class="form-control" name="customer['dataNascimento']" value="<?php echo $customer['dataNascimento']; ?>" required>
     </div>
   </div>
   <div class="row">
     <div class="form-group col-md-5">
       <label for="campo1">Endereço</label>
-      <input type="text" class="form-control" name="customer['endereco']" value="<?php echo $customer['endereco']; ?>">
+      <input type="text" class="form-control" name="customer['endereco']" value="<?php echo $customer['endereco']; ?>"required>
     </div>
 
     <div class="form-group col-md-3">
       <label for="campo2">Bairro</label>
-      <input type="text" class="form-control" name="customer['bairro']" value="<?php echo $customer['bairro']; ?>">
+      <input type="text" class="form-control" name="customer['bairro']" value="<?php echo $customer['bairro']; ?>" required>
     </div>
 
     <div class="form-group col-md-2">
       <label for="campo3">CEP</label>
-      <input type="text" class="form-control" name="customer['CEP']" value="<?php echo $customer['CEP']; ?>">
+      <input type="text" class="form-control" name="customer['CEP']" value="<?php echo $customer['CEP']; ?>" required>
     </div>
 
     <div class="form-group col-md-2">
       <label for="campo3">Data de Cadastro</label>
-      <input type="date" class="form-control" name="customer['created']" disabled value="<?php echo $customer['created']; ?>">
+      <input type="date" class="form-control" name="customer['created']" disabled value="<?php echo $customer['created']; ?>"required>
     </div>
   </div>
   <div class="row">
     <div class="form-group col-md-3">
       <label for="campo1">Município</label>
-      <input type="text" class="form-control" name="customer['cidade']" value="<?php echo $customer['cidade']; ?>">
+      <input type="text" class="form-control" name="customer['cidade']" value="<?php echo $customer['cidade']; ?>" required>
     </div>
 
     <div class="form-group col-md-2">
       <label for="campo2">Telefone</label>
-      <input type="text" class="form-control" name="customer['telefoneFixo']" value="<?php echo $customer['telefoneFixo']; ?>">
+      <input type="text" class="form-control" name="customer['telefoneFixo']" value="<?php echo $customer['telefoneFixo']; ?>" required>
     </div>
 
     <div class="form-group col-md-2">
       <label for="campo3">Celular</label>
-      <input type="text" class="form-control" name="customer['celular']" value="<?php echo $customer['celular']; ?>">
+      <input type="text" class="form-control" name="customer['celular']" value="<?php echo $customer['celular']; ?>" required>
     </div>
 
     <div class="form-group col-md-1">
       <label for="campo3">UF</label>
-      <input type="text" class="form-control" name="customer['estado']" value="<?php echo $customer['estado']; ?>">
+      <input type="text" class="form-control" name="customer['estado']" value="<?php echo $customer['estado']; ?>" required>
     </div>
 
     <div class="form-group col-md-2">
       <label for="campo3">Inscrição Estadual</label>
-      <input type="text" class="form-control" name="customer['ie']" value="<?php echo $customer['ie']; ?>">
+      <input type="text" class="form-control" name="customer['ie']" value="<?php echo $customer['ie']; ?>" required>
     </div>
 
    
