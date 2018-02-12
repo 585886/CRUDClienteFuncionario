60ce1b5bd056ed0f06e7eb515eb9666645915860
Alan Vinicius Developer <alan123vinicius@gmail.com>
2018-02-12 00:16:03 -0200

    Add files via upload

--- inc/footer.php
+++ inc/footer.php
@@ -1,10 +1,11 @@
 </main> <!-- /container -->
-<footer>
+<div class = "footer">
 	<a href ="http://www.alanvinicius.com" target="_blank">&copy;2017 - Alan Vinicius</a>
-</footer>
+</div>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="<?php echo BASEURL; ?>js/jquery-1.11.2.min.js"><\/script>')</script>
 <script src="<?php echo BASEURL; ?>js/bootstrap.min.js"></script>
 <script src="<?php echo BASEURL; ?>js/main.js"></script>
+</script>
 </body>
 </html>
